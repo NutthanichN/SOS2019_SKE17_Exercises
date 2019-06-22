66de617119dada1b32e1f99ea648f2bcae0abca9
Nutthanich Narphromar <monaprom134@gmail.com>
2019-06-23 00:42:29 +0700

    edit (again)

--- ex45_average.py
+++ ex45_average.py
@@ -1,5 +1,5 @@
 def average(lst):
     for i in lst:
         if not isinstance(i, int):
-            return "This is not a list of integer"
+            return "This is not a list of integers."
     return sum(lst) / len(lst)
