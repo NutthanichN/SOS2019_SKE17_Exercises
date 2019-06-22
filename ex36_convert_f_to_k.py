2cc527f1542b033c0a35b5cd0ecd58ceb49bba83
Nutthanich Narphromar <monaprom134@gmail.com>
2019-06-22 16:48:36 +0700

    edit

--- ex36_convert_f_to_k.py
+++ ex36_convert_f_to_k.py
@@ -1,8 +1,3 @@
 def fahrenheit_to_kelvin(fahrenheit):
     kelvin = (fahrenheit + 459.67) * (5/9)
     return round(kelvin, 2)
-
-
-fahrenheit = float(input("Enter temperature (in Fahrenheit): "))
-kelvin = fahrenheit_to_kelvin(fahrenheit)
-print(f"{fahrenheit:.2f} Fahrenheit = {kelvin:.2f} Kelvin")
