a272105d042279b1ba885fa457f80d5c89dcdee4
Nutthanich Narphromar <monaprom134@gmail.com>
2019-06-26 22:36:33 +0700

    edit non-negative integer

--- ex21_prime_number.py
+++ ex21_prime_number.py
@@ -1,17 +1,24 @@
-"""Is prime numbers?"""
+"""Is a prime number?
+
+positive integer = [1, inf)
+non-negative integer = [0, inf)
+negative integer = (-inf, -1]
+non-positive integer = (-inf, 0]
+
+"""
 
 number = int(input("Number: "))
 factors = []
 
 if number < 0:
-    print("Input number must be a positive integer.")
+    print("Input number must be a non-negative integer.")
 else:
     for i in range(1, number + 1):
         if number % i == 0:
             factors.append(i)
 
     if len(factors) == 2:
-        print(f"{number} is prime numbers.")
+        print(f"{number} is a prime number.")
     else:
-        print(f"{number} is not prime numbers.")
+        print(f"{number} is not a prime number.")
 
