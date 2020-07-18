dca066887695bafeed215d55bd2826e35d2c5fd1
Nutthanich Narphromar <monaprom134@gmail.com>
2020-07-18 21:49:52 +0700

    Edit error message

--- ex18_zodiac.py
+++ ex18_zodiac.py
@@ -29,89 +29,103 @@ month = input("Enter your birth month(Jan/Feb/Mar/Apr/May/Jun/Jul/Aug/Sep/Oct/No
 # except KeyError:
 #     print("This month does not exist.")
 
+day_does_not_exist_msg = "This date does not exist."
+
 if month == "Jan":
     if 0 < day <= 19:
         print("Your zodiac sign is Capricorn.")
     elif 19 < day <= 31:
         print("Your zodiac sign is Aquarius.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Feb":
     if 0 < day <= 18:
         print("Your zodiac sign is Aquarius.")
     elif 18 < day <= 29:
         print("Your zodiac sign is Pisces.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Mar":
     if 0 < day <= 20:
         print("Your zodiac sign is Pisces.")
     elif 20 < day <= 31:
         print("Your zodiac sign is Aries.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Apr":
     if 0 < day <= 19:
         print("Your zodiac sign is Aries.")
     elif 19 < day <= 30:
         print("Your zodiac sign is Taurus.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "May":
     if 0 < day <= 20:
         print("Your zodiac sign is Taurus.")
     elif 20 < day <= 31:
         print("Your zodiac sign is Gemini.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Jun":
     if 0 < day <= 20:
         print("Your zodiac sign is Gemini.")
     elif 20 < day <= 30:
         print("Your zodiac sign is Cancer.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Jul":
     if 0 < day <= 22:
         print("Your zodiac sign is Cancer.")
     elif 22 < day <= 31:
         print("Your zodiac sign is Leo.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Aug":
     if 0 < day <= 22:
         print("Your zodiac sign is Leo.")
     elif 22 < day <= 31:
         print("Your zodiac sign is Virgo.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Sep":
     if 0 < day <= 22:
         print("Your zodiac sign is Virgo.")
     elif 22 < day <= 30:
         print("Your zodiac sign is Libra.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Oct":
     if 0 < day <= 22:
         print("Your zodiac sign is Libra.")
     elif 22 < day <= 31:
         print("Your zodiac sign is Scorpio.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Nov":
     if 0 < day <= 21:
         print("Your zodiac sign is Scorpio.")
     elif 21 < day <= 30:
         print("Your zodiac sign is Sagittarius.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 elif month == "Dec":
     if 0 < day <= 21:
         print("Your zodiac sign is Sagittarius.")
     elif 21 < day <= 31:
         print("Your zodiac sign is Capricorn.")
     else:
-        print("There is no this day in this month.")
+        # print("There is no this day in this month.")
+        print(day_does_not_exist_msg)
 else:
     print("This month does not exist.")
