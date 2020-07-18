6879cb6d55881a6e0c2f7ba37cb9fb4ca0c48550
Nutthanich Narphromar <monaprom134@gmail.com>
2020-07-18 22:15:34 +0700

    Edit message

--- ex34_election.py
+++ ex34_election.py
@@ -3,4 +3,4 @@
 age = int(input("How old are you? "))
 while age < 18:
     age = int(input("How old are you? "))
-print("Congratulation! You can vote in the election.")
+print("Congratulations! You can vote in the election.")
